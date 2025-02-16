e6ebb06a716271c02942d069efd9a2074da9e2a7
rajesh <ajoypal.raj@gmail.com>
2025-02-16 19:52:37 +0600

    course-details page

--- src/components/student/TestimonialsSection.jsx
+++ src/components/student/TestimonialsSection.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { assets, dummyTestimonial } from "../../assets/assets";
 const TestimonialsSection = () => {
-  console.log(dummyTestimonial);
   return (
     <div className="pb-14 px-8 md:px-0">
       <h2 className="text-3xl font-medium text-gray-800">Testimonials</h2>
